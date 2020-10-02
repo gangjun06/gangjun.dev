79fcc911d8c88960201db6d88cddc65822808b3c
gangjun06 <me@gangjun.dev>
2020-10-02 14:00:46 +0000

    apply Fonts, Intro Design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,38 @@
 import React, { FC } from 'react'
 
 const Home: FC = () => {
-  return <div>Home</div>
+  return (
+    <div className=''>
+      <BuildIntro />
+      <BuildAbout />
+    </div>
+  )
+}
+
+const BuildIntro = () => {
+  return (
+    <div id='intro'>
+      <div className='container mx-auto'>
+        <div className='flex'>
+          <div className='bg-color-4 text-black px-2 py-1 rounded'>
+            Full-Stack Developer
+          </div>
+        </div>
+        <div className='mt-8 tracking-wide text-4xl'>
+          <p>Talk is cheap.</p>
+          <p>Show me the code</p>
+        </div>
+        <div className="mt-4 font-color-5">
+          <p>I develop a lot of interesting things.</p>
+          <p>And I love what I do.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const BuildAbout = () => {
+  return <div id='about' className='bg-color-3'></div>
 }
 
 export default Home

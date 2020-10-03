d90d6f99c8c7ae5603fa9717c5637f0eb8e2dc0e
gangjun06 <gangjun06@users.noreply.github.com>
2020-10-03 12:17:36 +0000

    Automated deployment: Sat Oct  3 12:17:36 UTC 2020 223d26ee743be5785398c3784aadac90fb178e10

---
+++ precache-manifest.c2f003424e9a4b4bc44f33dafea6957c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e864fe59359534d6a292402e8afa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b375ee1714854e02ec2a",
+    "url": "/static/css/main.47697406.chunk.css"
+  },
+  {
+    "revision": "d2a6546d4e789a05debf",
+    "url": "/static/js/2.b4c23bf9.chunk.js"
+  },
+  {
+    "revision": "fd4f7fee6c09809dea2e8620aaac2f85",
+    "url": "/static/js/2.b4c23bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b375ee1714854e02ec2a",
+    "url": "/static/js/main.7c1c4af8.chunk.js"
+  },
+  {
+    "revision": "2e9ff60e9e05ef1bfe07",
+    "url": "/static/js/runtime-main.edf6ca41.js"
+  },
+  {
+    "revision": "f4b2058b3fec0be6adff617619a5adf1",
+    "url": "/static/media/IntroImg.f4b2058b.svg"
+  }
+]);
